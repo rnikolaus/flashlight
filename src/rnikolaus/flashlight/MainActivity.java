494edfcbb12cca5bb1c67898ab9c66c4df186c61
rnikolaus <rnikolaus@users.noreply.github.com>
2014-11-08 17:02:42 +0100

    bugfix

--- src/rnikolaus/flashlight/MainActivity.java
+++ src/rnikolaus/flashlight/MainActivity.java
@@ -2,7 +2,6 @@ package rnikolaus.flashlight;
 
 import java.util.List;
 
-
 import android.support.v7.app.ActionBarActivity;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
@@ -19,78 +18,91 @@ public class MainActivity extends ActionBarActivity {
 
 	/**
 	 * Set the flashlight state according to the toggleButton
+	 * 
 	 * @param v
 	 */
 	public void switchFlashlight(View v) {
+		toggleButtonAction();
+	}
+
+	private void toggleButtonAction() {
 		ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton1);
 		flashlight(toggleButton.isChecked());
 	}
 
 	/**
 	 * Switch the flashlight on or off
+	 * 
 	 * @param on
 	 */
 	public void flashlight(boolean on) {
-		Parameters p = mCamera.getParameters();
-		if (on) {
-			if (!p.getFlashMode().equals(Parameters.FLASH_MODE_TORCH)) {
-				p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-				mCamera.setParameters(p);
+		try {
+			Parameters p = mCamera.getParameters();
+			if (on) {
+				if (!p.getFlashMode().equals(Parameters.FLASH_MODE_TORCH)) {
+					p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+					mCamera.setParameters(p);
+				}
+			} else {
+				if (p.getFlashMode().equals(Parameters.FLASH_MODE_TORCH)) {
+					p.setFlashMode(Parameters.FLASH_MODE_OFF);
+					mCamera.setParameters(p);
+				}
 			}
-		} else {
-			if (p.getFlashMode().equals(Parameters.FLASH_MODE_TORCH)) {
-				p.setFlashMode(Parameters.FLASH_MODE_OFF);
-				mCamera.setParameters(p);
-			}
+		} catch (RuntimeException ex) {
+			// A screen orientation change during flashing morse code signals
+			// might end here,
+			// as the flash might be used before the camera is reinitialized
 		}
 	}
-	
-	
+
 	/**
-	 * This will flash out the pattern described by the Boolean list
-	 * One timeunit has a duration of 100 ms
+	 * This will flash out the pattern described by the Boolean list One
+	 * timeunit has a duration of 100 ms
+	 * 
 	 * @param signals
 	 */
-	public void flashPattern(final List<Boolean> signals){
-		if (blinkThread!=null){
+	public void flashPattern(final List<Boolean> signals) {
+		if (blinkThread != null) {
 			blinkThread.interrupt();
 		}
-		blinkThread = new Thread(){
+		blinkThread = new Thread() {
 			@Override
 			public void run() {
-				for (Boolean signal:signals){
-					if (isInterrupted()){//die gracefully
+				for (Boolean signal : signals) {
+					if (isInterrupted()) {// die gracefully
 						break;
 					}
 					flashlight(signal);
 					try {
 						sleep(100);
-					} catch (InterruptedException e) {//die gracefully
+					} catch (InterruptedException e) {// die gracefully
 						break;
 					}
 				}
+				toggleButtonAction();
 			}
-			
+
 		};
 		blinkThread.start();
-		
+
 	}
 
 	/**
-	 * Turns the content of the editText into a signal pattern and plays #
-	 * it through the camera flash
+	 * Turns the content of the editText into a signal pattern and plays # it
+	 * through the camera flash
+	 * 
 	 * @param v
 	 */
 	public void flash(View v) {
-		EditText editText= (EditText)findViewById(R.id.editText1);
+		EditText editText = (EditText) findViewById(R.id.editText1);
 		Morse morse = new Morse();
-		List<Boolean> signals = morse.messageToSignal(editText.getText().toString());
+		List<Boolean> signals = morse.messageToSignal(editText.getText()
+				.toString());
 		flashPattern(signals);
-		
+
 	}
 
-	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -98,24 +110,26 @@ public class MainActivity extends ActionBarActivity {
 		if (mCamera == null) {
 			mCamera = Camera.open();
 		}
-
 	}
 
-	
 	@Override
 	public void onPause() {
-		super.onPause(); 
-		if (blinkThread!=null){
-			blinkThread.interrupt();//stop blinking
+		super.onPause();
+		if (blinkThread != null) {
+			blinkThread.interrupt();// stop blinking
 		}
 		if (mCamera != null) {
-			mCamera.release();//release the cam to other apps
+			mCamera.release();// release the cam to other apps
 			mCamera = null;
 		}
-		
-		
+
 	}
 
-
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		toggleButtonAction();
+	}
 
 }
